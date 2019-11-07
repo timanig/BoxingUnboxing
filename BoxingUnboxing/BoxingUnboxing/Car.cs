2cbba1068d08eb031a4db9d512c39ec130ff2d1f
Timani G <timanig@gmail.com>
2019-11-07 10:09:02 -0500

    /Boxing examples

---
+++ BoxingUnboxing/BoxingUnboxing/Car.cs
@@ -0,0 +1,27 @@
+﻿namespace BoxingUnboxing
+{
+    public class Car
+    {
+        public Car()
+        {
+        }
+
+        string[] car = new string[40];
+
+        public string this[int carNum]
+        {
+            get
+            {
+                if (carNum >= 0 && carNum < car.Length)
+                    return car[carNum];
+                else
+                    return "Out of index range";
+            }
+            set
+            {
+                if (carNum >= 0 && carNum < car.Length)
+                    car[carNum] = value;
+            }
+        }
+    }
+}
---
+++ BoxingUnboxing/BoxingUnboxing/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //var var0 = 3;
+            //int val1 = 4;
+            //float val2 = val1;
+            //int val3 = (int)val2;
+
+            //Console.WriteLine(val2);
+
+            //double result;
+            //string str = "abc";
+            //Double.TryParse(str, out result);
+
+            //Console.WriteLine(result);
+
+            int x = 25;
+            object o =x; //boxing using implicit conversion
+           
+            if (o is int val)
+            {
+                Console.WriteLine(val);
+            }
+
+
+
+            //var array = new int[] { 10,11,12};
+
+            //Console.WriteLine(array[0]);
+
+
+            //var list = new List<int>();
+            //list.Add(10);
+            //list.Add(11);
+            //list.Add(12);
+            //Console.WriteLine(list);
+
+
+
+            var carList = new Car();
+            carList[1] = "Porsche";
+            carList[2] = "Lamborghini";
+            Console.WriteLine(carList[41]);
+
+
+
+
+        }
+    }
+}
